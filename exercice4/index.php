29d87e403b8e9ef3224eb4cf50e2e3f85e5a775e
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-25 14:49:29 +0200

    exercice 4 fini

---
+++ exercice4/index.php
@@ -0,0 +1,19 @@
+<?php 
+    $departements = [
+        02 => 'Aisne',
+        59 => 'Nord',
+        60 => 'Oise',
+        62 => 'Pas-de-Calais',
+        80 => 'Somme',
+    ];
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8"/>
+    <title>Exercice 4 PHP</title>
+</head>
+<body>
+
+</body>
+</html>
