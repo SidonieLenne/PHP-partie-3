32c3fe63d173255bcd642d2b467d08294ca363a6
SidonieLenne <lenne.sidonie@gmail.com>
2020-05-25 11:06:47 +0200

    exercice 2 fini

---
+++ exercice2/index.php
@@ -0,0 +1,28 @@
+<?php 
+    $months = [
+        1 => 'Janvier',
+        2 => 'Février',
+        3 => 'Mars',
+        4 => 'Avril',
+        5 => 'Mai',
+        6 => 'Juin',
+        7 => 'Juillet',
+        8 => 'Août',
+        9 => 'Septembre',
+        10 => 'Octobre',
+        11 => 'Novembre',
+        12 => 'Décembre'
+    ];
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8"/>
+    <title>Exercice 2 PHP</title>
+</head>
+<body>
+    <p><?php
+        echo $months[5];
+    ?></p>
+</body>
+</html>
